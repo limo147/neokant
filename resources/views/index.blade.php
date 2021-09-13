fd4de953dfb310f3e585cb5d625fb667a34ae797
limo147 <linzhe470@gmail.com>
2021-09-13 11:07:18 +0800

    meta

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -193,6 +193,7 @@ canvas {
   }
 }
         </style>
+        <meta name="google-site-verification" content="__1l5ceJQHoVz_6wDXmoO8EuLWpMWOGKrTv0Zsh6FRU" />
     </head>
     <body>
         <div style="padding:20px">
