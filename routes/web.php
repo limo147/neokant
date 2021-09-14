adf7fec600601ded74882f347c7390c7f4f1eff1
limo147 <88359290+limo147@users.noreply.github.com>
2021-09-14 09:07:54 +0800

    put public

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,6 @@ Route::get('/google198c15353f62bb77.html', function () {
     return view('google198c15353f62bb77');
 });
 
-Route::get('/newsitemap.xml', function () {
-    
-    $xmlString = file_get_contents(public_path('newsitemap.xml'));
-    $xmlObject = simplexml_load_string($xmlString);
-               
-    $json = json_encode($xmlObject);
-    $phpArray = json_decode($json, true); 
-
-    dd($phpArray);
-});
 
 Auth::routes();
 
